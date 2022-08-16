7c68748a5f8c92125847d7a1752ec2e8b9872a95
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-08-16 21:46:39 +0200

    Exceptions

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,15 @@ use emu_kernel::println;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
   println!("Hello World{}", "!");
+
+  emu_kernel::init();
+
+  x86_64::instructions::interrupts::int3();
   
   #[cfg(test)]
   test_main();
 
+  println!("It did not crash!");
   loop {}
 }
 
